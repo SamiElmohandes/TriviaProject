0b8fb7897fac1a3a5bd74782f87d81e99f583636
SamiElmohandes <32831255+SamiElmohandes@users.noreply.github.com>
2020-03-26 15:58:26 +0200

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import urllib
 import json
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,7 +16,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = 'postgresql://postgres@localhost:5432/trivia_test'
+        #"postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -59,8 +61,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data["success"], True)
 
     def test_delete_question(self):
-        question = Question(question='Test', answer='Test',
-                            difficulty=3, category=2)
+        question = Question(question='Test', answer='Test', difficulty=3, category=2)
         question.insert()
         question_id = question.id
 
@@ -68,13 +69,17 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['Deleted'], question_id)
+        self.assertEqual(data['deleted'], question_id)
 
 
     def test_add_question(self):
-        question = Question(question='Test', answer='Test',
-                            difficulty=3, category=2)
-        
+        #question = Question(question='Test', answer='Test',difficulty=3, category=2)
+        question={
+                   'question': 'mock question',
+                   'answer': 'mock answer',
+                   'category': 1,
+                   'difficulty': 2,
+                  }
         res = self.client().post('/questions', json=question)
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
@@ -83,13 +88,40 @@ class TriviaTestCase(unittest.TestCase):
 
     def test_get_questions_by_category(self):
         category = Category.query.first()
-        res = self.client().get('/categories/{}/questions').format(category.id)
+        res = self.client().get('/categories/{}/questions'.format(category.id))
 
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['questions']))
+
+
+    
+    def test_get_questions_by_nonexistent_category(self):
+        res = self.client().get('/categories/12/questions')
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+
+
+     
+    def test_add_question_with_missing_fields(self):
+        #question = Question(question='Test', answer='Test',difficulty=3, category=2)
+        question={
+                   'question': 'mock question',
+                   'answer': 'mock answer',
+                   'category': 1,
+                   'difficulty': ''
+                  }
+        res = self.client().post('/questions', json=question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+
 
 
 # Make the tests conveniently executable
